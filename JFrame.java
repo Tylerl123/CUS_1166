a43c5d03b4311050d7c8a3a2e1d69a0ea27ddd18
Luis Guarin <92492074+lgtorres618@users.noreply.github.com>
2022-09-27 22:25:31 -0400

    Update JFrame.java

--- JFrame.java
+++ JFrame.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class JFrame extends JFrame implements ActionListener {
+public class MyFrame extends JFrame implements ActionListener {
   
   
   JButton button;
