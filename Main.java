01dda831946154090a34c102176c4596a0ae39dd
RyanDoii <107153508+RyanDoii@users.noreply.github.com>
2022-09-27 22:11:11 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Tyler");
+        JFrame frame = new JFrame();
+        JButton login = new JButton("Login");
 
     }
     
