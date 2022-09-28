e91c46be4b2ac2e487be476ad016d496075a565c
Luis Guarin <92492074+lgtorres618@users.noreply.github.com>
2022-09-27 22:18:13 -0400

    Create MyFrame.java

---
+++ MyFrame.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame implements ActionListener {
+  
+  
+  JButton button;
+  JButton button2;
+  
+ JTextField textField;
+  
+  frame(){
+    
+  
+  
+  
+  
+  
+}
+  @Override 
+  public void actionPerformed(ActionEvent e){
+    
+  }
+  
+}
+
+
+}
