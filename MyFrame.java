48c3a31884251794ae7166960aa5de2c4d28532f
Tylerl123 <90160215+Tylerl123@users.noreply.github.com>
2022-10-01 23:27:50 -0400

    Update MyFrame.java

--- MyFrame.java
+++ MyFrame.java
@@ -3,7 +3,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -12,10 +11,16 @@ public class MyFrame extends JFrame implements ActionListener {
   
   private JButton button;
   private JButton button2;
-  private static  JFrame frame;  
+  private static JFrame frame;  
   private JPanel panel;
   private JLabel label;
   private JTextField textField;
+  
+  public static JTextField clientUserName;
+  public static JTextField clientPassword;
+  public static JTextField clientFullName;
+  public static JTextField clientJobDuration;
+  public static JTextField clientDeadline;
   
   public static JTextField ownerUserName;
   public static JTextField ownerFullname;
@@ -23,15 +28,14 @@ public class MyFrame extends JFrame implements ActionListener {
   public static JTextField ownerCarModel;
   public static JTextField ownerCarYear;
   
-  
   public MyFrame(){
 	    frame = new JFrame("VCRTS Project");  //This creates the initial frame with the owner and user button
 	    frame.setSize(500,500);
 	    frame.setVisible(true);
 	    panel = new JPanel();
 	    frame.add(panel);
-	    button = new JButton("Rent Vehicle Hardware"); //Client rents a vehicle
-	    button2 = new JButton("Register Vehicle"); //Owner registers a car
+	    button = new JButton("Rent a vehicle"); //Client rents a vehicle
+	    button2 = new JButton("Register a Vehicle"); //Owner registers a car
 	    panel.add(button);
 	    panel.add(button2);
 	    button.addActionListener(this);
@@ -39,15 +43,28 @@ public class MyFrame extends JFrame implements ActionListener {
 	    button2.addActionListener(ownerLogin);
 	    
 		
+	  
+	    //textField = new JTextField();
+        //textField.setPreferredSize(new Dimension(250,40));
+        //textField.setFont(new Font("Courier New",Font.PLAIN,15));	//This is the text field that goes along with the initial frame
+        
   }
+
+
+
+
         // Action performed method that gives functionality to java "back button"
         @Override 
         public void actionPerformed(ActionEvent e) { // create new window for owner to login
               if(e.getSource()==button) {
                 createClientButton();
                 frame.dispose();
+            
           }
+              
         }
+        
+        
         
 
 
@@ -55,9 +72,32 @@ public class MyFrame extends JFrame implements ActionListener {
         
         JFrame frame2 = new JFrame("Client Login");
         JPanel panel2 = new JPanel();
-        JLabel label2 = new JLabel("Username");
-        JLabel labelP = new JLabel("Password");
-		JLabel labelFN = new JLabel("Full Name");
+        
+        JButton enterButton2 = new JButton(new AbstractAction("Enter!") { //make back button to go to previous window
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    BufferedWriter bw = new BufferedWriter(new FileWriter("clientInformation.txt",true));
+                    bw.append("UserName: " + clientUserName.getText() + "\n");
+                    bw.append("Password:  " + clientPassword.getText() + "\n");
+                    bw.append("Client Full Name:  " + clientFullName.getText() + "\n");
+                    bw.append("Client Job Duration " + clientJobDuration.getText() + "\n");
+                    bw.append("Client Deadline " + clientDeadline.getText() + "\n");
+
+                    bw.close();
+
+
+                }catch(IOException ex){
+
+                }
+
+            }
+        });
+        
+        JLabel label2 = new JLabel("Username");  
+	    JLabel labelP = new JLabel("Password");
+	    JLabel labelFN = new JLabel("Full Name");
         JLabel labelTime = new JLabel("Job Duration");
         JLabel labelDeadline = new JLabel("Deadline");
         JButton back = new JButton(new AbstractAction("Back!") {
@@ -69,38 +109,39 @@ public class MyFrame extends JFrame implements ActionListener {
                 }
         });
           
-        JTextField textField2 = new JTextField();//textfield for username
-        textField2.setPreferredSize(new Dimension(400,40));
-        textField2.setFont(new Font("Consolas",Font.PLAIN,15));
+        clientUserName = new JTextField();//textfield for username
+        clientUserName.setPreferredSize(new Dimension(400,40));
+        clientUserName.setFont(new Font("Consolas",Font.PLAIN,15));
 		
-        JPasswordField passTextField = new JPasswordField();//textfield for password
-        passTextField.setPreferredSize(new Dimension(400,40));
+        clientPassword = new JPasswordField();//textfield for password
+        clientPassword.setPreferredSize(new Dimension(400,40));
 		
-        JTextField textField3 = new JTextField();//textfield for full name
-        textField3.setPreferredSize (new Dimension(400, 40));
+        clientFullName = new JTextField();//textfield for full name
+        clientFullName.setPreferredSize (new Dimension(400, 40));
         
-        JTextField textField4 = new JTextField();//textfield for job duration
-        textField4.setPreferredSize (new Dimension(400, 40));
+        clientJobDuration = new JTextField();//textfield for job duration
+        clientJobDuration.setPreferredSize (new Dimension(400, 40));
         
-        JTextField textField5 = new JTextField();//textfield for deadline
-        textField5.setPreferredSize (new Dimension(400, 40));
+        clientDeadline = new JTextField();//textfield for deadline
+        clientDeadline.setPreferredSize (new Dimension(400, 40));
 		
 		// labels added to the panel
         panel2.add(label2);
-        panel2.add(textField2);
+        panel2.add(clientUserName);
 		
-        panel2.add(labelP);
-        panel2.add(passTextField);
+	panel2.add(labelP);
+        panel2.add(clientPassword);
 		
-        panel2.add(labelFN);
-        panel2.add(textField3);
+	panel2.add(labelFN);
+        panel2.add(clientFullName);
 		
         panel2.add(labelTime);
-        panel2.add(textField4);
+        panel2.add(clientJobDuration);
         
         panel2.add(labelDeadline);
-        panel2.add(textField5);
+        panel2.add(clientDeadline);
 		
+        panel2.add(enterButton2);
         panel2.add(back);
         frame2.add(panel2);
         frame2.setSize(500,500);
@@ -110,7 +151,7 @@ public class MyFrame extends JFrame implements ActionListener {
 	
 	
 
-class addOwnerLogin implements ActionListener{
+	  class addOwnerLogin implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -122,44 +163,44 @@ class addOwnerLogin implements ActionListener{
 		}
 	}
 
+
 public static void createOwnerButton() { //Beginning creating the owner button with username
 
 		JFrame frame3 = new JFrame("Owner Login");
 		JPanel panel3 = new JPanel();
 		JLabel label3 = new JLabel("Username");
-		panel3.setBackground(Color.PINK);
+		JButton enterButton = new JButton(new AbstractAction("Enter!") { //make back button to go to previous window
 
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    BufferedWriter bw = new BufferedWriter(new FileWriter("OwnerInformation.txt",true));
+                    bw.append("UserName: " + ownerUserName.getText() + "\n");
+                    bw.append("Full Name:  " + ownerFullname.getText() + "\n");
+                    bw.append("Car Make:  " + ownerCarMake.getText() + "\n");
+                    bw.append("Car Model: " + ownerCarModel.getText() + "\n");
+                    bw.append("Car Year: " + ownerCarYear.getText() + "\n");
+
+                    bw.close();
+
+
+                }catch(IOException ex){
+
+                }
+
+            }
+        });
 		
 		JLabel labelFN = new JLabel("Full Name");	//Information collected from vehicle owner
 		JLabel labelVMake= new JLabel("Vehicle Make");
 		JLabel labelVModel = new JLabel("Vehicle Model");
 		JLabel labelVYear= new JLabel("Vehicle Year");
-
-		JButton enterButton = new JButton(new AbstractAction("Enter!") { //make back button to go to previous window
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try{
-		            BufferedWriter ownerLogin = new BufferedWriter(new FileWriter("OwnerInformation.txt",true));
-		            ownerLogin.append("UserName: " + ownerUserName.getText() + "\n");
-		            ownerLogin.append("Full Name:  " + ownerFullname.getText() + "\n");
-		            ownerLogin.append("Car Make:  " + ownerCarMake.getText() + "\n");
-		            ownerLogin.append("Car Model: " + ownerCarModel.getText() + "\n");
-		            ownerLogin.append("Car Year: " + ownerCarYear.getText() + "\n");
-		          
-		            ownerLogin.close();
-		            
-		            
-		        }catch(IOException ex){
-		        	
-		        }
-			
-			}
-		});
+		
+	
 		JButton back = new JButton(new AbstractAction("Back!") { //make back button to go to previous window
 
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent back) {
 				// TODO Auto-generated method stub
 				frame.setVisible(true);
 				frame3.dispose();
@@ -167,21 +208,21 @@ public static void createOwnerButton() { //Beginning creating the owner button w
 			}
 		});
 		
-			ownerUserName = new JTextField();//textfield for username
-			ownerUserName.setPreferredSize(new Dimension(450,40));
-			ownerUserName.setFont(new Font("Consolas",Font.PLAIN,18));
+		ownerUserName = new JTextField();//textfield for username
+		ownerUserName.setPreferredSize(new Dimension(450,40));
+		ownerUserName.setFont(new Font("Consolas",Font.PLAIN,18));
 		
-       	 	ownerFullname = new JTextField();//textfield for fullname
-        	ownerFullname.setPreferredSize (new Dimension(450, 40));
+		ownerFullname = new JTextField();//textfield for fullanem
+		ownerFullname.setPreferredSize (new Dimension(450, 40));
         
-        	ownerCarMake = new JTextField();//textfield for vehicle make
-        	ownerCarMake.setPreferredSize (new Dimension(450, 40));
+		ownerCarMake = new JTextField();//textfield for vehicle make
+		ownerCarMake.setPreferredSize (new Dimension(450, 40));
         
-        	ownerCarModel = new JTextField();//textfield for vehicle model
-        	ownerCarModel.setPreferredSize (new Dimension(450, 40));
+		ownerCarModel = new JTextField();//textfield for vehicle model
+		ownerCarModel.setPreferredSize (new Dimension(450, 40));
         
-        	ownerCarYear = new JTextField();//textfield for vehicle year
-       		ownerCarYear.setPreferredSize (new Dimension(450, 40));
+		ownerCarYear  = new JTextField();//textfield for vehicle year
+		ownerCarYear.setPreferredSize (new Dimension(450, 40));
 	
 	//adding each label to the panel created up top
 		panel3.add(label3);
@@ -205,9 +246,18 @@ public static void createOwnerButton() { //Beginning creating the owner button w
 		frame3.setSize(500,500);
 		frame3.setVisible(true);
 	}
- 
+        
+
+
 
 //Save Data in a file
+public static void saveFile(String data) throws IOException
+{
+String fileName = new SimpleDateFormat("yyyyMMddHHmm'.txt'").format(new java.util.Date());
+BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+writer.write(data);
+writer.close();
+System.out.println("File Saved");
 
 }
-
+}
