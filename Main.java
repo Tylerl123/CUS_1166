186491597397cce65ce59b8197366d45b551fb19
Tylerl123 <90160215+Tylerl123@users.noreply.github.com>
2022-10-04 21:15:17 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
+/* Project:  Milestone 2
+* Class: HomeScreen.java 
+* Author:   Ryan Doyle, Luis Guarin, Tyler Logan, Dondre Campbell, Shafin Ahmed
+* Date: October 4, 2022 
+* This program sets the initial frame
+*/ 
+
+
 public class Main {
     public static void main(String[] args) {
         new HomeScreen();
