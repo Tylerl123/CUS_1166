79ca76a221776bbb8a0a848277004b5a2c76b4c0
Luis Guarin <92492074+lgtorres618@users.noreply.github.com>
2022-10-28 21:30:28 -0400

    Update Job.java

--- Job.java
+++ Job.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.io.*;
+import java.nio.file.Files;
+import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
@@ -8,6 +10,8 @@ import java.util.Random;
 
 public class  Job extends Main {
     public static int JobID;
+    static int INTCOMPLETION;
+    static String JAB;
 
     public Job() {
 
@@ -19,17 +23,19 @@ public class  Job extends Main {
 
             BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\Luis\\CUS1166 GUI\\Job.txt", true));
 
-            bw.append("Job ID:" + JobID + "\n");
+            //bw.append("Job ID:" + JobID + "\n");
 
-            bw.append("Job Duration:" + HomeScreen.clientJobDuration.getText() + "\n");
-           
+           bw.append(HomeScreen.clientJobDuration.getText() + "\n");
+
+            JAB = HomeScreen.clientJobDuration.getText();
+            INTCOMPLETION = Integer.parseInt(JAB);
+
 
             bw.close();
 
         } catch (IOException ex) {
 
         }
-
         ArrayList<String> JobList = new ArrayList<String>();
 
         try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Luis\\CUS1166 GUI\\Job.txt"))) {
@@ -37,13 +43,34 @@ public class  Job extends Main {
 
             while ((JobIndex = br.readLine()) != null) {
                 JobList.add(JobIndex);
+
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.print(JobList + "\n");
+        List<Integer> newList = new ArrayList<Integer>(JobList.size());
+        for (String myInt : JobList) {
+            newList.add(Integer.valueOf(myInt));
+            int COMPLETION = 0;
+            for (int i = 0; i < newList.size(); i++) {
+
+                COMPLETION += newList.get(i);
+
+
+            }
+            System.out.println(COMPLETION);
+        } try {
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\Luis\\CUS1166 GUI\\Job.txt", true));
+
+            //bw.append("Job ID:" + JobID + "\n");
+
+
+
+            bw.close();
+
+        } catch (IOException ex) {
+
+        }
     }
-
-}
-
