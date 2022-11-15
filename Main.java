324084cef77fd68a27d9bf698051356c254cbb50
Luis Guarin <92492074+lgtorres618@users.noreply.github.com>
2022-11-14 22:34:17 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,36 @@
-/* Project:  Milestone 2
-* Class: Main.java
-* Author:   Ryan Doyle, Luis Guarin, Tyler Logan, Dondre Campbell, Shafin Ahmed
-* Date: October 4, 2022 
-* This program sets the initial frame
-*/ 
-
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        new HomeScreen();
-        
+
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Thread t1 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    HomeScreen.main(args);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t1.start();
+        Thread t2 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    Server.main(args);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t2.start();
+
     }
+
 }
