dc02d0b4d5d21d2031ee1c013d404ef6ec61b4ba
Luis Guarin <92492074+lgtorres618@users.noreply.github.com>
2022-11-16 10:51:24 -0500

    Update Client.java

--- Client.java
+++ Client.java
@@ -11,66 +11,37 @@ public class Client extends Main {
         public static String deadLine;
         public static String JobIDString;
         public static String UserName;
-
         public static String duration;
-
-
         public static void APPENDCLIENT() throws IOException {
 
+                new Job();
                 DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH: mm :ss");
                 LocalDateTime now = LocalDateTime.now();
                 String DateTimer = (dtf.format(now));
                 System.out.println("Completed Successfully");
-
-
                 try {
-
                         BufferedWriter bw = new BufferedWriter(new FileWriter("clientInformation.txt", true));
-                     bw.append(Server.clientInput.toString());
+                        if (Client.UserName==null);
 
-
-                      //  bw.append("Client ID: " + HomeScreen.clientUserName.getText() + "\n");
-                      //  bw.append("Password:  " + HomeScreen.clientPassword.getText() + "\n");
-                      //  bw.append("Client Full Name:  " + HomeScreen.clientFullName.getText() + "\n");
-
-                        //  bw.append("Client Job Duration " + clientJobDuration.getText() + "  hours " + "\n");
-
-
-                      //  bw.append("Client Deadline " + HomeScreen.clientDeadline.getText() + "\n");
-                       // bw.append("Job ID:" + HomeScreen.JobIDField.getText() + " finishes at Duration:" + HomeScreen.clientJobDuration.getText() + "\n");
-                       // bw.append(DateTimer + "\n");
-
-                        bw.close();
-                        JOptionPane.showMessageDialog(null, "The request has been accepted", "Client Login", JOptionPane.PLAIN_MESSAGE);
-                        //JFrame SusWindow = new JFrame("ACCEPTED");
-                       // SusWindow.setSize(450,400);
-                       // SusWindow.setLayout(new BoxLayout(SusWindow.getContentPane(),BoxLayout.Y_AXIS));
-                       // SusWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                        //SusWindow.setVisible(true);
+                                else{
+                                bw.append(Server.clientInput.toString() +"\n");
+                                bw.append(DateTimer + "\n");
+                                bw.close();
+                                JOptionPane.showMessageDialog(null, "The request has been accepted for:  " + Client.UserName + " Job ID " + Client.JobIDString, "Client Login", JOptionPane.PLAIN_MESSAGE);
+                        }
                 } catch (IOException ex) {
-
                 }
-
-               // HomeScreen.clientUserName.setText("");
-               // HomeScreen.clientPassword.setText("");
-               // HomeScreen.clientFullName.setText("");
-               // HomeScreen.JobIDField.setText("");
-               // HomeScreen.clientJobDuration.setText("");
-
-
-               // HomeScreen.clientDeadline.setText("");
-
-
-
         }
-
         public static void REJECTCLIENT() throws IOException {
-                System.out.println("Rejected");
+                if (Client.UserName==null);
 
+                else {
+                        System.out.println("Rejected");
+                        JOptionPane.showMessageDialog(null, "The request sent by Client:  " + Client.UserName + " Full Name: " + Client.fullName + " was rejected by Admin", " Client Login ", JOptionPane.PLAIN_MESSAGE);
+                }
         }
 
 }
-
 
 
 
