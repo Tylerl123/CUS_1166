acacb3bd61def70e58c90df9682962aceb97cc31
Luis Guarin <92492074+lgtorres618@users.noreply.github.com>
2022-11-16 10:52:44 -0500

    Update Owner.java

--- Owner.java
+++ Owner.java
@@ -1,1 +1,60 @@
+import javax.swing.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Owner extends Main {
+
+    public static String fullName;
+    public static String carMake;
+    public static String carYear;
+    public static String carResidency;
+    public static String UserName;
+    public static String carModel;
+
+
+
+    public static void APPENDOWNER() throws IOException {
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH: mm :ss");
+        LocalDateTime now = LocalDateTime.now();
+        String DateTimer = (dtf.format(now));
+        System.out.println("Completed Successfully");
+
+        try {
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter("OwnerInformation.txt", true));
+
+            if (Owner.UserName == null);
+
+            else {
+
+
+                bw.append(Server.ownerInput.toString());
+                bw.append(DateTimer + "\n");
+                JOptionPane.showMessageDialog(null, "The request has been accepted for:  " + Owner.UserName +" Name:  " + Owner.fullName , "Owner Login", JOptionPane.PLAIN_MESSAGE);
+                bw.close();
+            }
+
+
+
+        } catch (IOException ex) {
+
+        }
+
+    }
+
+    public static void REJECTOWNER() throws IOException {
+        if (Owner.UserName==null);
+
+            else {
+
+            System.out.println("Rejected");
+            JOptionPane.showMessageDialog(null, "The request sent by Owner:  " + Owner.UserName + " Full Name: " + Owner.fullName + " was rejected by Admin", " Owner Login ", JOptionPane.PLAIN_MESSAGE);
+        }
+    }
+
+}
 
