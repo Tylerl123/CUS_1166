ab5fb93aba370f2d160b6ffa6073d8bdc825ee98
Ryan <ryandoyle1289@gmail.com>
2022-11-29 22:59:00 -0500

    reger

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
-
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.Statement;
 public class Main {
 
-
+public static Connection connection = null;
+public static String url = "jdbc:mysql://127.0.0.1:3306/?user=root";
+public static String username = "root";
+public static String password = "ar1560ds";
     public static void main(String[] args) throws IOException, InterruptedException {
         Thread t1 = new Thread(new Runnable() {
 
@@ -30,7 +36,6 @@ public class Main {
             }
         });
         t2.start();
-
     }
-
 }
+
